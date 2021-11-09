143a76c8b9b244a0ddab7b7326f495c13cb95d7a
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-11-09 17:55:26 +0100

    tried to figure out how to insert HTML in content

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,18 @@ p 'Seeding started'
 
 10.times do
   faker_title = Faker::Company.catch_phrase
-  faker_content = Faker::Company.bs.capitalize
+  faker_content = []
+  rand(1..3).times do
+    content = <<-HTML
+    <h2>#{Faker::Marketing.buzzwords.capitalize}</h2>
+    <p>#{Faker::Lorem.paragraphs.join}</p>
+    HTML
+    faker_content << content
+  end
 
-  article = Article.new(title: faker_title, content: faker_content)
+  faker_content =
+
+  article = Article.new(title: faker_title, content: faker_content.join)
   article.save
 end
 
